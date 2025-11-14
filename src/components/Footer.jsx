776c80b52c0ec5f7093c5843b1f71dfb98b63499
karan daksh <karan@quokkalabs.com>
2025-11-14 20:24:40 +0530

    left menu open close toggling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,16 +9,20 @@ const Footer = () => {
     <>
       Footer Component
 
-      {/* <p className='text-center'>
+      <p className='text-center'>
 
         <span>{counter}</span>
 
-        <button className='mx-4 px-4 py-2 border' onClick={() => setCounter(counter => counter >= 20 ? counter  : counter + 1)}>plus</button>
-        <button className='mx-4 px-4 py-2 border' onClick={() => setCounter(counter => counter === 0 ? counter : counter - 1)}>minus</button>
+        {/* <button className='mx-4 px-4 py-2 border' onClick={() => setCounter(counter => counter >= 20 ? counter : counter + 1)}>plus</button> */}
+        {/* <button className='mx-4 px-4 py-2 border' onClick={() => setCounter(counter => counter === 0 ? counter : counter - 1)}>minus</button> */}
+        
+        
+        
+        {/* <button className='mx-4 px-4 py-2 border' onClick={() => setCounter(prev => prev === 0 ? prev : prev-1)}>minus</button> */}
       </p>
 
 
- */}
+
 
 
     </>
