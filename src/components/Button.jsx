50ea77db47b63a2adbe08ed6963c0af33a6bc003
karan daksh <karan@quokkalabs.com>
2025-11-18 23:53:41 +0530

    Data get the API and strore in the global Array

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   return (
     <button
       className={`bg-[var(--redColor)] text-white text-base px-9 py-3.5 rounded hover:opacity-70 inline-block cursor-pointer ${props.className}`}
-      onClick={props.onClick} // forward the click event
+      onClick={props.onClick || console.log("Working Button")} // forward the click event
     >
       {props.btnText}
     </button>
